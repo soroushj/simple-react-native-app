ed2194754c89a0ef6b4782a636c18f1f063b5182
Soroush Javadi <soroush.javadi@gmail.com>
2019-06-03 15:55:02 +0430

    Setup Enzyme

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
+  preset: 'react-native',
   roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.tsx?$': 'babel-jest',
   },
-  preset: 'react-native',
+  setupFilesAfterEnv: ['<rootDir>/scripts/setup-tests.js'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
 };
